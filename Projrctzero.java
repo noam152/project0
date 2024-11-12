da987a23a9437514624b1f48a8d83cc2e60ee6f8
noamb <noam.braun@gmail.com>
2024-11-12 12:23:05 +0200

    update

---
+++ Projrctzero.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Projrctzero {
+
+    //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+    public class Main {
+        public static void main(String[] args) {
+            System.out.println(("please enter a random number"));
+            int a = 2;
+            int b = 3;
+            if (a + b == number) {
+                System.out.println(a + b + "=" + number);
+            }
+            else {
+                while(a += b != number){
+
+                }
+
+            }
+        }
+    }
+}
